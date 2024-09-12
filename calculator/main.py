17a8668cd29cac853130bd9d048a4f31150148b4
OfficialCrak <chirjeveg@gmail.com>
2024-09-12 20:23:07 +0700

    feat: adding action selection menu "main" and adding a function "switch_case" without logic

--- calculator/main.py
+++ calculator/main.py
@@ -18,3 +18,30 @@ def divide(x: float, y: float) -> float:
 
 def change_sign(x: float) -> float:
     return -x
+
+
+def switch_case(number: int) -> None:
+    pass
+
+
+def main() -> None:
+    while True:
+        print(
+            "\nПростой калькулятор на Python \n"
+            "Выберите действие:\n"
+            "1 - Сложить два числа\n"
+            "2 - Вычесть из первого числа второе\n"
+            "3 - Умножить два числа\n"
+            "4 - Разделить первое число на второе\n"
+            "5 - Изменить знак\n"
+            "0 - Выход\n"
+        )
+        number = int(input("Введите номер действия: "))
+        if number == 0:
+            print("Выход из программы...")
+            break
+        switch_case(number)
+
+
+if __name__ == '__main__':
+    main()
